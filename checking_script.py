50e24ded8147687b2025a556caf36b72d0b533ca
Tony Kviat <antonkviatofficial@gmail.com>
2018-09-13 02:46:24 +0300

    Added loading bar; handled timeout except

--- checking_script.py
+++ checking_script.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3.5
 
 import cgi, cgitb, random, string, json, os
-from subprocess import run, PIPE, Popen, STDOUT
+from subprocess import run, PIPE, Popen, STDOUT, TimeoutExpired
 import xml.etree.ElementTree as ET
 
 def check_code(return_code):
     if return_code == 0:
         return "success"
+    elif return_code == 2:
+        return "process_timeout"
     else:
         return "failed"
 
 cgitb.enable()
 
 print ("Content-Type: application/json, charset=utf-8\r\n\r\n")
-#print ("Content-Type: text/html, charset=utf-8\r\n\r\n")
+# print ("Content-Type: text/html, charset=utf-8\r\n\r\n")
 data = cgi.FieldStorage()
 
 sourceCode = data['srcCode'].value
@@ -35,25 +37,30 @@ compile_output = proc_desc.stdout.read()
 compile_output = compile_output.decode('utf-8').replace("\n", "<br>")
 # print("return code: " + str(compile_return_code))
 # print("compile output: " + compile_output)
-with open("/usr/lib/cgi-bin/wtf.txt", "w") as text_file:
-    text_file.write(compile_output)
+# with open("/usr/lib/cgi-bin/wtf.txt", "w") as text_file:
+#     text_file.write(compile_output)
 
 valgrind_error_code = 1
 valgrind_return_code = valgrind_error_code
 errors_str = ''
 if compile_return_code == 0:
     valgrind_args = ("valgrind", "--leak-check=full", "--xml=yes", "--error-exitcode=" + str(valgrind_error_code), "--xml-file=" + fileName + ".xml", "./" + fileName)
-    valgrind_desc = run(valgrind_args, stdout=PIPE, stderr=STDOUT, input=std_input.encode(), timeout=3)
-    valgrind_return_code = valgrind_desc.returncode
+    try:
+        valgrind_desc = run(valgrind_args, stdout=PIPE, stderr=STDOUT, input=std_input.encode(), timeout=3)
+        valgrind_return_code = valgrind_desc.returncode
 
-    exec_stdout = valgrind_desc.stdout
+        exec_stdout = valgrind_desc.stdout
 
-    tree = ET.parse(fileName + ".xml")
-    root = tree.getroot()
-    errorsList = []
-    for error in root.findall("error"):
-        errorsList.append(error.find("xwhat").find("text").text)
-    errors_str = "<br>".join(errorsList)
+        tree = ET.parse(fileName + ".xml")
+        root = tree.getroot()
+        errorsList = []
+        for error in root.findall("error"):
+            errorsList.append(error.find("xwhat").find("text").text)
+        errors_str = "<br>".join(errorsList)
+    except TimeoutExpired as inst:
+        errors_str = "Process timeout error. May be you forgot to remove blocking operations?"
+        valgrind_return_code = 2
+
     
 json_str = json.dumps([{"build_result" : check_code(compile_return_code), "build_log" : compile_output},
                         {"valgrind_result" : check_code(valgrind_return_code), "valgrind_log" : errors_str} ])
