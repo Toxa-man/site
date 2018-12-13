00c6c60c5dc9133480fb865901b72933a0f4ba90
Tony Kviat <antonkviatofficial@gmail.com>
2018-12-13 03:21:06 +0300

    Removed lab_7

--- html/js/build_table.js
+++ html/js/build_table.js
@@ -1,18 +1,20 @@
 function build_table(selector) {
     $.getJSON("data.json", function(data) {
         // console.log(data);
+        var lab_count = 6;
         var tr_tag = $('<tr/>');
         tr_tag.append($('<th/>').attr('scope', 'col').text('FIO'));
-        for (i = 1; i<=7; i++){
+        for (i = 1; i < lab_count; i++){
             tr_tag.append($('<th/>').attr('scope', 'col').addClass('text-center').text('Лаб. ' + i.toString()));
         }
+        tr_tag.append($('<th/>').attr('scope', 'col').addClass('text-center').text('Курсовая'));
         $(selector).append($('<thead/>').append($(tr_tag)));
         
         var tbody = $('<tbody/>');
         for (const [key, value] of Object.entries(data)) {
             var tr_tag = $('<tr/>');
             tr_tag.append($('<th/>').attr('scope', 'row').text(key));
-            for (i = 1; i<=7; i++){
+            for (i = 1; i <= lab_count; i++){
                 if (value.indexOf(i) > -1){
                     tr_tag.append($('<th/>').addClass('text-center').attr('scope', 'col').text('+'));
                 } else {
