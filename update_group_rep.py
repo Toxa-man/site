bdccb1f4e0daca82dc0b6c61955d70401a8c23a5
Toxa_man <antonkviatofficial@gmail.com>
2019-03-01 19:39:47 +0300

    Fixed file opening

--- update_group_rep.py
+++ update_group_rep.py
@@ -39,8 +39,9 @@ for dirname in sorted(os.listdir(os.getcwd())):
         mark = ""
         fio = ""
         os.chdir(dirname)
-        with open("fio.txt", mode="r") as text_file:
-            fio = text_file.read().replace("\n", "")
+		if os.path.isfile("fio.txt"):
+			with open("fio.txt", mode="r") as text_file:
+				fio = text_file.read().replace("\n", "")
         for i in range(1, course_lab_id + 1):
             if os.path.isdir("lab_" + str(i)):
                 labs_done.append(i)
